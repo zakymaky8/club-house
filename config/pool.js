a3925b79fa303f174085f87e2cb9a0985e365d2e
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-11-19 22:29:34 +0300

    add connection string

--- config/pool.js
+++ config/pool.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 
 module.exports = new Pool({
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT,
-    host: "localhost",
-    password: process.env.PASSWORD
+    // user: process.env.USER,
+    // database: process.env.DATABASE,
+    // port: process.env.DB_PORT,
+    // host: "localhost",
+    // password: process.env.PASSWORD
+    connectionString: process.env.POSTGRES_URL
 })
